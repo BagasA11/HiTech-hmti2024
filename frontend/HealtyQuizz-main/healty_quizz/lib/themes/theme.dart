66b27340997c75ef955563fd91040d3fb3d07a14
ryokf <ryokhrisnaf@gmail.com>
2024-04-17 21:23:41 +0700

    add theme.dart

---
+++ frontend/HealtyQuizz-main/healty_quizz/lib/themes/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF12372A);
+const Color secondaryColor = Color(0xFFADBC9F);
+const Color accentColor = Color(0xFFFBFADA);
+const Color blackColor = Color(0xFF333333);
+const Color whiteColor = Color(0xFFffffff);
+const Color greyColor = Color(0xFFaaaaaa);
