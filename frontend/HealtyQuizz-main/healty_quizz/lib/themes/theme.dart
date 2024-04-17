01858bf370771cbbbad0a84d86a9f4b5f7e5aea2
RAYNF <rayhannoor33@gmail.com>
2024-04-17 23:16:27 +0700

    updateTheme
    update perubahan style ke theme sampai halaman login page

--- frontend/HealtyQuizz-main/healty_quizz/lib/themes/theme.dart
+++ frontend/HealtyQuizz-main/healty_quizz/lib/themes/theme.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-const Color primaryColor = Color(0xFF12372A);
-const Color secondaryColor = Color(0xFFADBC9F);
-const Color accentColor = Color(0xFFFBFADA);
-const Color blackColor = Color(0xFF333333);
+const Color primaryColor = Color(0xFFD2F6C5);
+const Color secondaryColor = Color(0xFF28DF99);
+const Color accentColor = Color(0xFFF00BF63);
+const Color blackColor = Color.fromARGB(255, 0, 0, 0);
 const Color whiteColor = Color(0xFFffffff);
+
 const Color greyColor = Color(0xFFaaaaaa);
+
+TextStyle title = GoogleFonts.poppins(fontSize: 20, color: whiteColor);
+TextStyle subTitle = GoogleFonts.poppins(fontSize: 14, color: blackColor);
+TextStyle preSubTitles =
+    GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w600);
+
+TextStyle heading =
+    GoogleFonts.openSans(fontSize: 27, fontWeight: FontWeight.bold);
+TextStyle subHeading =
+    GoogleFonts.openSans(fontSize: 20, fontWeight: FontWeight.bold);
+TextStyle text =
+    GoogleFonts.openSans(fontSize: 16, fontWeight: FontWeight.w200);
+TextStyle subText = GoogleFonts.openSans(fontSize: 12);
