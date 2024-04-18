93580f1899851cbf27328b0fd490654e26648f75
RAYNF <rayhannoor33@gmail.com>
2024-04-18 23:26:39 +0700

    addMakeQuizPage
    didnt edit page just add page

--- frontend/HealtyQuizz-main/healty_quizz/lib/presentation/pages/register_page.dart
+++ frontend/HealtyQuizz-main/healty_quizz/lib/presentation/pages/register_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:healty_quizz/presentation/pages/user/home_user_page.dart';
+import 'package:healty_quizz/presentation/pages/home_user_page.dart';
 import 'package:healty_quizz/presentation/pages/login_page.dart';
 import 'package:healty_quizz/themes/theme.dart';
 
