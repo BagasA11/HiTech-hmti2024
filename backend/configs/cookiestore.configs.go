d978c0d4765f332818533c2bbac4ae5bf18b0aaf
BagasA112 <bagasrayhan03@gmail.com>
2024-04-23 16:47:26 +0700

    delete /backend

--- backend/configs/cookiestore.configs.go
+++
@@ -1,9 +0,0 @@
-package configs
-
-import (
-	"github.com/gorilla/sessions"
-)
-
-var Store = sessions.NewCookieStore([]byte("asdaskdhasdhgsajdgasdsadksakdhasidoajsdousahdopj"))
-
-// var Store = ""
